2177e8c6c30c39bb7aefea1a3893babab39de7c5
Arthur Gibert <arthurgibert@free.fr>
2024-04-10 08:36:09 +0200

    View Accueil.html

--- src/main/java/fr/ldnr/dao/ArticleRepository.java
+++ src/main/java/fr/ldnr/dao/ArticleRepository.java
@@ -1,11 +1,13 @@
 package fr.ldnr.dao;
 
+
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.ldnr.entities.Article;
 
-import java.awt.print.Pageable;
+
 
 public interface ArticleRepository extends JpaRepository<Article, Long>{
     Page<Article> findByDescriptionContains(String description, Pageable pageable);
--- src/main/java/fr/ldnr/web/ArticleController.java
+++ src/main/java/fr/ldnr/web/ArticleController.java
@@ -5,12 +5,12 @@ import fr.ldnr.entities.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Controller
@@ -22,10 +22,17 @@ public class ArticleController {
     @GetMapping("/index")
     public String index(Model model, @RequestParam(name="page", defaultValue = "0") int page, @RequestParam(name="keyword", defaultValue= "") String kw) {
         Page<Article> articles = articleRepository.findByDescriptionContains(kw, (Pageable) PageRequest.of(page, 5));
+        model.addAttribute("keyword", kw);
         model.addAttribute("listArticle", articles.getContent());
         model.addAttribute("pages", new int[articles.getTotalPages()]);
         model.addAttribute("currentPage", page);
 
         return "articles";
+    }
+
+    @GetMapping("/delete")
+    public String delete(Long id, int page, String keyword) {
+        articleRepository.deleteById(id);
+        return "redirect:/index?page=" + page + "&keyword=" + keyword;
     }
 }
